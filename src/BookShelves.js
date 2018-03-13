5a69827223ee8167569bc937eaed188aafc69d55
Aqueum <martin.currie@aqueum.com>
2018-03-13 20:45:04 +0000

    style: Format shelves

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -8,13 +8,17 @@ const shelfNames = [
 class BookShelves extends Component {
   render() {
     return (
-      <div>
-        <h1>MyReads</h1>
-        {shelfNames.map(shelfName => (
-          <div className="bookshelf" key={shelfName.name}>
-            <h2 className="bookshelf-title">{shelfName.name}</h2>
-          </div>
-        ))}
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          {shelfNames.map(shelfName => (
+            <div className="bookshelf" key={shelfName.name}>
+              <h2 className="bookshelf-title">{shelfName.name}</h2>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
