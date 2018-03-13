724bb3f2af31612f7fa6473ac0c7f70d988b5432
Aqueum <martin.currie@aqueum.com>
2018-03-13 20:34:36 +0000

    feat: Add shelf names

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+const shelfNames = [
+  { name: 'Currently Reading' },
+  { name: 'Want to Read' },
+  { name: 'Read' }
+];
 class BookShelves extends Component {
   render() {
     return (
       <div>
         <h1>MyReads</h1>
+        {shelfNames.map(shelfName => (
+          <div className="bookshelf" key={shelfName.name}>
+            <h2 className="bookshelf-title">{shelfName.name}</h2>
+          </div>
+        ))}
       </div>
     );
   }
