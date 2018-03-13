fa572a524d079fc4bdecc82aa96ff223895600f3
Aqueum <martin@aqueum.com>
2018-03-13 17:09:07 +0000

    fix: Remove state page control

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    books: []
+  };
 
   render() {
-    return (
-      <div className="app">
-
-      </div>
-    )
+    return <div className="app" />;
   }
 }
 
-export default BooksApp
+export default BooksApp;
