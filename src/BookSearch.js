d2f3d2a97fff2af883e3ce5c0e9ea39919d18b0e
Aqueum <martin.currie@aqueum.com>
2018-03-14 14:39:22 +0000

    feat: Add search functionality

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import BookListings from './BookListings';
+import * as BooksAPI from './BooksAPI';
 
 class BookSearch extends Component {
+  state = {
+    books: [],
+    query: ''
+  };
+
+  updateQuery = query => {
+    this.setState({ query: query.trim() });
+    BooksAPI.search(this.state.query).then(books => {
+      this.setState({ books });
+    });
+  };
+
   render() {
+    const { query, books } = this.state;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -18,10 +34,17 @@ class BookSearch extends Component {
                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                 you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={event => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
-        <div className="search-books-results">Books go here</div>
+        <div className="search-books-results">
+          <BookListings books={books} />
+        </div>
       </div>
     );
   }
