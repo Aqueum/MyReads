fabeaf8c5e23ba185febf7fe3a37f4d6387aab83
Aqueum <martin.currie@aqueum.com>
2018-03-14 19:31:03 +0000

    fix: Handle no results

--- src/BookListings.js
+++ src/BookListings.js
@@ -4,7 +4,7 @@ import BookMove from './BookMove';
 
 class BookListings extends Component {
   static propTypes = {
-    books: PropTypes.array.isRequired,
+    books: PropTypes.array,
     onChangeShelf: PropTypes.func.isRequired
   };
   render() {
@@ -19,25 +19,30 @@ class BookListings extends Component {
 
     return (
       <ol className="books-grid">
-        {showingBooks.map(book => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                  }}
-                />
-                <BookMove book={book} onChangeShelf={onChangeShelf} />
+        {showingBooks !== undefined &&
+          showingBooks.map(book => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${
+                        book.imageLinks !== undefined
+                          ? book.imageLinks.smallThumbnail
+                          : ''
+                      })`
+                    }}
+                  />
+                  <BookMove book={book} onChangeShelf={onChangeShelf} />
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))}
       </ol>
     );
   }
